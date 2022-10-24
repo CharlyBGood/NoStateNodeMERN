f7a7b330fe88547fee1f37b8698ca1b861f8dc35
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-10-24 20:18:28 -0300

    create data models

---
+++ backend/src/models/User.js
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("User", userSchema);
